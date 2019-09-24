86aad237fbec36d7cce41a8145bf8c0335eb0282
Seba <omgl0lwth@gmail.com>
2019-09-24 18:10:23 +0200

    mapping methods and parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,6 +5,11 @@
     public function __construct(){
         echo "Pages loaded";
     }
+    public function about($id){
+        echo $id;
+    }
+    public function index(){
 
+    }
 
  }
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -25,6 +25,21 @@
         require_once('../app/controllers/' . $this->currentController . '.php');
 
         $this->currentController = new $this->currentController;
+
+        if (isset($url[1]) ){
+            if(method_exists( $this->currentController, $url[1] )){
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // get params 
+
+        $this->params = $url ? array_values($url) : [];
+
+        // call a callback with array of params
+
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl(){
@@ -37,9 +52,6 @@
             return $url;
 
         }
-        
-
-
     }
 
  }
