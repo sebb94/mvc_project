4ab57c86066d5d8a80a3dcda25ef54551b72e7de
Seba <omgl0lwth@gmail.com>
2019-09-28 12:08:12 +0200

    edit post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -65,6 +65,57 @@
       
     }
 
+     public function edit($id){
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = [
+                'id'   => $id,
+                'title' => trim($_POST['title']),
+                'body'  => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            if(empty($data['title'])){
+                $data['title_err'] = "Please enter title";
+            }
+            if(empty($data['body'])){
+                $data['body_err'] = "Please enter text";
+            }
+
+            if( empty($data['title_err']) && empty($data['body_err'])){
+               if($this->postModel->updatePost($data)){
+                flash('post_message', 'Post edited');
+                redirect('posts');
+               }else{
+                die ("Something went wrong");
+               }
+
+            }else{
+                $this->view('posts/edit', $data);
+            }
+
+
+        }else{
+
+            $post = $this->postModel->getPostById($id);
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+
+           $data = [
+            'id'    => $id,
+            'title' => $post->title,
+            'body'  => $post->body
+         ];
+         $this->view('posts/edit', $data);
+        }
+      
+    }
+
     public function show($id){
 
         $post = $this->postModel->getPostById($id);
